8e416f6117b061d45accddd625831e556c4ada81
xp2000926 <77706310+xp2000926@users.noreply.github.com>
2022-09-07 20:07:01 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import "min-smarty-ui-vite/entry.css";
+import "min-smarty-ui-vite/index.css";
 import MinSmartyUI from "min-smarty-ui-vite";
 
 createApp(App).use(MinSmartyUI).mount("#app");
